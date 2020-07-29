bbf13972d6b7dc77386411b5bed052fb03c5e66d
LucasSte <lucas.tnagel@gmail.com>
2020-07-29 11:28:51 -0300

    Adding netowrk statistics

--- iOSMonitor/MainVC.swift
+++ iOSMonitor/MainVC.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2020 Lucas Steuernagel. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
 class MainVC: UIViewController {
@@ -15,11 +16,18 @@ class MainVC: UIViewController {
     @IBOutlet weak var totalMem : UILabel!
     @IBOutlet weak var usedMem : UILabel!
     @IBOutlet weak var execPath : UILabel!
+    var networkInfo = DataUsageInfo()
+    
+    @IBOutlet weak var wifiSent: UILabel!
+    @IBOutlet weak var wifiRec: UILabel!
+    @IBOutlet weak var cellSent : UILabel!
+    @IBOutlet weak var cellRec: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.cpuMonitor.updateInfo(self.cpuMonitor.updateTimer)
         changeText()
+
     }
     
     func changeText()
@@ -32,16 +40,21 @@ class MainVC: UIViewController {
         DispatchQueue.global(qos: .background).async {
             while(true)
             {
-                    var text : String = ""
-                    for i in 0 ..< self.cpuMonitor.numCPUs
-                    {
-                        text = text + "Core " + i.description + ": " + String(format: "%.2f",self.cpuMonitor.coreUsage[Int(i)]) + "%\n"
-                    }
-                    DispatchQueue.main.async {
-                        self.coreUsage.text = text
-                        self.usedMem.text = String(format: "%.2f", self.memMonitor.usedMemory()! / self.memMonitor.totalMemory() * 100)
-                    }
-                    sleep(1)
+                self.networkInfo.updateInfoByAdding(info: DataUsage.getDataUsage())
+                var text : String = ""
+                for i in 0 ..< self.cpuMonitor.numCPUs
+                {
+                    text = text + "Core " + i.description + ": " + String(format: "%.2f",self.cpuMonitor.coreUsage[Int(i)]) + "%\n"
+                }
+                DispatchQueue.main.async {
+                    self.coreUsage.text = text
+                    self.usedMem.text = String(format: "%.2f", self.memMonitor.usedMemory()! / self.memMonitor.totalMemory() * 100)
+                    self.wifiSent.text = self.networkInfo.wifiSent.description
+                    self.wifiRec.text = self.networkInfo.wifiReceived.description
+                    self.cellRec.text = self.networkInfo.wirelessWanDataReceived.description
+                    self.cellSent.text = self.networkInfo.wirelessWanDataSent.description
+                }
+                sleep(1)
             }
             
         }
